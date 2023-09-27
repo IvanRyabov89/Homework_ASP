bdbdbbb7a7989e92f3338b85f596e79fbe0681bc
IvanRyabov89 <138253067+IvanRyabov89@users.noreply.github.com>
2023-09-27 21:10:43 +0300

    Add files via upload

---
+++ Project.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Homework_ASP
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string NumberTelephone { get; set; }
+        public string Address { get; set; }
+        public string Destruction { get; set; }
+    }
+}
